754d5db5a33dcea57c30c1732c83fdb9dc93dee9
CarlosEduardo31 <carlos.eduardo.3832@gmail.com>
2021-02-10 08:45:10 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,111 @@
+let valor1 = parseInt(gets(1));
+let valor2 = parseInt(gets(2));
+let total = valor1 + valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);
+
+/*
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+
+function redirecionar(){
+    window.open("https://google.com.br"); //abrir em outra janela
+   // window.location.href = "https://google.com.br"; Abri na mesma janela
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //document.getElementById("mouseover").innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mouseover").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagica carregada");
+}
+
+function funcaochanger(elemento){
+    console.log(elemento.value)
+}*/
+
+/*****
+function soma(n1,n2){
+    return n1+n2;
+};
+console.log(soma(10,5));
+
+function validaIdade(idade){
+    var validar;
+    if(idade>=18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+*****/
+
+/*****
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+var count;
+for(count=0; count<=5; count++){
+    console.log(count);
+};
+
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+};
+
+var idade = prompt("Qual sua idade;")
+if(idade>=18) {
+    alert("Maior de idade");
+}else {
+    alert("Menor de idade");
+};
+*****/
+
+/*****
+var frutas =[{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+  
+var lista = ["maça", "pêra", "laranja"];
+lista.push("Uva"); //Colocar na lista
+lista.pop() // remove ultimo elemento do array
+console.log(lista);
+console.log(lista[1]);
+console.log(lista.length); //Mostra quantos elementos tem no array
+console.log(lista.reverse()); //Mostra os elementos do array do ultimo ao primeiro
+console.log(lista.toString());
+console.log(lista.join("|"));
+*****/
+
+/*****
+var nome = "Carlos Eduardo";
+var idade = 27;
+var frase = "Japão é o melhor time do mundo";
+
+alert(nome + " tem " + idade + " anos.");
+console.log(idade);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+*****/
